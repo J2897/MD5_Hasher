5a9168aab9aa5ad8d889bbd94372fa86dca0f5b0
John Shaw <j2897@mailinator.com>
2013-07-22 10:13:52 +0100

    First commit.

---
+++ MD5_Hasher.bat
@@ -0,0 +1,49 @@
+@echo OFF
+pushd "%~dp0"
+cls
+
+REM Get the OS #-Bit of the OS...
+set BIT=64
+if %PROCESSOR_ARCHITECTURE% == x86 (
+	if not defined PROCESSOR_ARCHITEW6432 (set BIT=32)
+)
+
+REM Set the appropriate md5deep executable...
+if %BIT% == 64 (
+	(set MD5DEEP_EXE="%CD%\md5deep\md5deep64.exe") 
+) else (
+	(set MD5DEEP_EXE="%CD%\md5deep\md5deep.exe")
+)
+
+set FILES_2_B_HASHED="%CD%\hashed"
+set HASH_DB="%CD%\hashes.dat"
+set TEMP_HASH_DB="%CD%\temp_hashes.dat"
+
+REM Create a FILES_2_B_HASHED folder if necessary...
+if not exist %FILES_2_B_HASHED% (
+	(md %FILES_2_B_HASHED%)
+)
+
+echo Place the "files to be hashed" inside this folder...
+echo %FILES_2_B_HASHED%
+echo.
+start %WINDIR%\explorer.exe %FILES_2_B_HASHED%
+pause
+
+REM Generate a HASH_DB file from the files in the FILES_2_B_HASHED folder...
+if exist %HASH_DB% (
+	(%MD5DEEP_EXE% -X %HASH_DB% -b -r %FILES_2_B_HASHED% > %TEMP_HASH_DB%)
+) else (
+	(%MD5DEEP_EXE% -b -r %FILES_2_B_HASHED% > %HASH_DB%) && (goto :End)
+)
+
+REM Append the Hashes in the TEMP_HASH_DB file to the HASH_DB file...
+type %TEMP_HASH_DB% >> %HASH_DB%
+
+REM Delete the TEMP_HASH_DB file...
+del %TEMP_HASH_DB%
+
+:End
+popd
+
+start %WINDIR%\system32\notepad.exe %HASH_DB%
---
+++ README.md
@@ -0,0 +1,7 @@
+MD5_Hasher
+==========
+*Released under the GNU General Public License version 3 by J2897.*
+
+The batch file uses [md5deep] [1] to generate, and enables you to easily add to, a list of MD5 hashes in a text file ('hashes.dat').
+
+   [1]: http://md5deep.sourceforge.net/
